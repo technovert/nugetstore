b8b6b5c526e25e91985c8ca48947ea5a7a112c73
Ranadheer.Khyatam <ranadheerk@technovert.com>
2019-07-30 17:31:17 +0530

    added samp;e registrations

---
+++ DependencyInjection.Autofac/content/DependencyBuilder.cs
@@ -0,0 +1,30 @@
+﻿using DependencyInjection.AutofacHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $rootnamespace$
+{
+    public class DependencyBuilder
+{
+    public static void Init()
+    {
+        AutofacDIHelper.Init(typeof(DependencyBuilder).Assembly);
+
+
+        /*dependency registration - START*/
+
+        //AutofacDIHelper.Register<MyService>();
+        //AutofacDIHelper.RegisterAs<MyService, IMyService>();
+        //AutofacDIHelper.RegisterAs<MyService, IMyService>(Lifetime.Scoped);
+        //AutofacDIHelper.RegisterAs<MyService, IMyService>(Lifetime.Singleton);
+        //AutofacDIHelper.RegisterAs<MyService, IMyService>(Lifetime.Transient);
+        
+        /*dependency registration - END*/
+
+        AutofacDIHelper.Build();
+    }
+}
+}
